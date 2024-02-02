4ea15e66eed419c55ec614806522390ded064315
sandro-silva-de-paula <sandrosilvadepaula.usa@gmail.com>
2024-02-02 16:04:43 -0500

    models...

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display= 'first_name',"last_name",'phone',
+    ordering = '-id',
+    list_filter = 'created_date',
+    search_fields='first_name',"last_name",
+    list_per_page=5
+    list_max_show_all = 200
+    list_display_links ='first_name',"last_name",
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(("Nome"), max_length=50)
+    last_name = models.CharField(("Sobrenome"), max_length=50, blank=True)
+    phone = models.CharField(("Telefone"), max_length=50)
+    email = models.EmailField(("Email"), max_length=254, blank=True)
+    created_date= models.DateTimeField(("Data Registro"),default=timezone.now)
+    description = models.TextField(("Comentarios"),blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
